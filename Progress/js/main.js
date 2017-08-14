ae2f13343e73d5ea644f01f7ac5a3c866700e06d
PYL <1874157401@qq.com>
2017-08-14 14:21:33 +0800

    add progress
    add progress

---
+++ Progress/js/main.js
@@ -0,0 +1,18 @@
+var progress = document.querySelector('.pyl-progress');
+//获取body高度&&浏览器高度&&滚动条距离顶部高度
+var _bodyH = document.body.scrollHeight;
+var _winH = window.innerHeight;
+var _scrollTop = 0;
+
+if(_bodyH - _winH <= 0){
+	progress.style.width = "100%";
+}
+
+
+// 滚动鼠标
+function mouseScroll(e){
+	_scrollTop = document.body.scrollTop
+	progress.style.width = (_scrollTop/(_bodyH - _winH)*100) + '%';
+}
+
+document.addEventListener('scroll',mouseScroll,false);
